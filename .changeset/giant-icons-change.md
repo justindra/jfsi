3a74f05dac095e58e82f87d51e569a9c9c19a6c7
Justin Rahardjo <justindra91@gmail.com>
2023-03-23 19:10:18 -0700

    fix: missing utils export in react

---
+++ .changeset/giant-icons-change.md
@@ -0,0 +1,6 @@
+---
+'@jfsi/react': patch
+'jfsi': patch
+---
+
+Export classnames from react and remove duplicate in jfsi
--- packages/jfsi/src/web/utils.ts
+++
@@ -1,8 +0,0 @@
-/**
- * This function is used to join class names together.
- * @param classes List of classes to join together.
- * @returns
- */
-export function classNames(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
-}

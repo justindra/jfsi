9a58112477fa123f99e20b1ad2d17334e566928b
Justin Rahardjo <justindra91@gmail.com>
2023-03-30 19:19:04 -0700

    feat: updated button to have loading state and icons

---
+++ .changeset/wicked-ants-doubt.md
@@ -0,0 +1,5 @@
+---
+'@jfsi/react': patch
+---
+
+Updated button to accept icon have a better loading state
--- packages/react/src/button.tsx
+++ packages/react/src/button.tsx
@@ -19,6 +19,8 @@ type ButtonProps = React.PropsWithChildren<{
   buttonProps?: React.ButtonHTMLAttributes<HTMLButtonElement>;
   disabled?: boolean;
   loading?: boolean;
+  startIcon?: React.FC<React.SVGProps<SVGSVGElement>>;
+  endIcon?: React.FC<React.SVGProps<SVGSVGElement>>;
 }>;
 
 export const Button: React.FC<ButtonProps> = ({
@@ -28,6 +30,8 @@ export const Button: React.FC<ButtonProps> = ({
   buttonProps = {},
   disabled,
   loading,
+  startIcon: StartIconComponent,
+  endIcon: EndIconComponent,
 }) => {
   return (
     <button
@@ -39,7 +43,38 @@ export const Button: React.FC<ButtonProps> = ({
         getColourClasses(variant, disabled || loading),
         buttonProps.className || ''
       )}>
-      {loading ? 'Loading...' : children}
+      {StartIconComponent && (
+        <StartIconComponent
+          className='-ml-0.5 mr-1.5 h-5 w-5'
+          aria-hidden='true'
+        />
+      )}
+      {loading && (
+        <svg
+          className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'>
+          <circle
+            className='opacity-25'
+            cx='12'
+            cy='12'
+            r='10'
+            stroke='currentColor'
+            strokeWidth='4'></circle>
+          <path
+            className='opacity-75'
+            fill='currentColor'
+            d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'></path>
+        </svg>
+      )}
+      {children}
+      {EndIconComponent && (
+        <EndIconComponent
+          className='-mr-0.5 ml-1.5 h-5 w-5'
+          aria-hidden='true'
+        />
+      )}
     </button>
   );
 };

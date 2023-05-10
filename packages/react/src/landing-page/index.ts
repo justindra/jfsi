a420fc70bd5800b07973c048cfdb33aa884b5cc4
Justin Rahardjo <justindra91@gmail.com>
2023-05-09 22:59:28 -0700

    feat: added hero with desktop screenshot

--- packages/react/src/landing-page/index.ts
+++ packages/react/src/landing-page/index.ts
@@ -2,6 +2,7 @@ export * from './cta';
 export * from './features';
 export * from './footer';
 export * from './hero';
+export * from './hero-desktop-screenshot';
 export * from './logo-cloud';
 export * from './pricing';
 export * from './testimonials';

00d83aa26c24a69d567849a48036a4e81c10ec75
Justin Rahardjo <justindra91@gmail.com>
2024-02-05 10:55:02 -0800

    feat: updated buttons

--- packages/react/src/button.tsx
+++ packages/react/src/button.tsx
@@ -12,29 +12,37 @@ type ButtonVariant =
 type ButtonSize = 'sm' | 'md' | 'lg';
 
 export function getColourClasses(
-  variant: ButtonVariant
+  variant: ButtonVariant,
+  dark: boolean
 ): NonNullable<React.HTMLAttributes<HTMLElement>['className']> {
-  if (variant === 'none') return 'ring-gray-300';
-  if (variant === 'primary') {
-    return 'bg-primary-600 hover:bg-primary-500 text-white ring-primary-500';
+  let className: string = '';
+  if (variant === 'none') {
+    if (dark) {
+      className = 'bg-transparent text-gray-50 hover:text-gray-300';
+    }
+    className = 'bg-transparent text-gray-900 ring-gray-300';
+  } else if (variant === 'primary') {
+    className =
+      '!bg-primary-600 hover:!bg-primary-500 text-white ring-primary-500';
+  } else if (variant === 'success') {
+    className = '!bg-green-600 hover:!bg-green-500 text-white ring-green-500';
+  } else if (variant === 'warning') {
+    className =
+      '!bg-yellow-600 hover:!bg-yellow-500 text-white ring-yellow-500';
+  } else if (variant === 'danger') {
+    className = '!bg-red-600 hover:!bg-red-500 text-white ring-red-500';
+  } else {
+    className = 'bg-white hover:bg-gray-50 text-gray-900 ring-gray-300';
   }
-  if (variant === 'success') {
-    return 'bg-green-600 hover:bg-green-500 text-white ring-green-500';
-  }
-  if (variant === 'warning') {
-    return 'bg-yellow-600 hover:bg-yellow-500 text-white ring-yellow-500';
-  }
-  if (variant === 'danger') {
-    return 'bg-red-600 hover:bg-red-500 text-white ring-red-500';
-  }
-  return 'bg-white hover:bg-gray-50 text-gray-900 ring-gray-300';
+
+  return classNames(className, dark ? '' : 'ring-1 ring-inset shadow-sm');
 }
 
 export function getSizeClasses(
   size: ButtonSize
 ): NonNullable<React.HTMLAttributes<HTMLElement>['className']> {
-  if (size === 'lg') return 'px-3.5 py-2.5';
-  return 'py-2 px-3';
+  if (size === 'lg') return 'px-3.5 py-2.5 text-base';
+  return 'py-2 px-3 text-sm';
 }
 
 const LoadingSpinner: React.FC = () => (
@@ -62,6 +70,7 @@ type BaseButtonProps = {
   size?: ButtonSize;
   disabled?: boolean;
   loading?: boolean;
+  dark?: boolean;
   startIcon?: HeroIcon;
   endIcon?: HeroIcon;
 };
@@ -82,6 +91,7 @@ export const Button: React.FC<ButtonProps> = ({
   buttonProps = {},
   disabled,
   loading,
+  dark = false,
   startIcon: StartIconComponent,
   endIcon: EndIconComponent,
   as: Component = 'button',
@@ -92,8 +102,8 @@ export const Button: React.FC<ButtonProps> = ({
       {...buttonProps}
       onClick={onClick}
       className={classNames(
-        'inline-flex items-center rounded-md text-sm font-semibold shadow-sm ring-1 ring-inset',
-        getColourClasses(variant),
+        'inline-flex items-center rounded-md font-semibold',
+        getColourClasses(variant, dark),
         getSizeClasses(size),
         disabled || loading ? 'opacity-50 pointer-events-none' : '',
         buttonProps.className || ''

d8d736f9c43bb5ec454172441684b3f1624bf5e5
Justin Rahardjo <justindra91@gmail.com>
2024-02-09 16:14:13 -0800

    feat: added disabled styling for text field

--- packages/react/src/forms/field-text.tsx
+++ packages/react/src/forms/field-text.tsx
@@ -38,7 +38,8 @@ export const FieldText = <
           'block w-full rounded-md border-0 py-1.5 dark:bg-white/5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset  placeholder:text-gray-400 dark:placeholder:text-initial focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
           error
             ? 'ring-red-500 focus:ring-red-500'
-            : 'ring-gray-300 dark:ring-white/10 focus:ring-primary-600 dark:focus:ring-primary-500'
+            : 'ring-gray-300 dark:ring-white/10 focus:ring-primary-600 dark:focus:ring-primary-500',
+          'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200'
         )}
         placeholder={props.placeholder}
       />

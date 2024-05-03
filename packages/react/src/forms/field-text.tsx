3eb611d5e0ce29b56030b7a8c58974b8eb3b679a
Justin Rahardjo <justindra91@gmail.com>
2024-05-03 15:29:40 -0700

    fix: missing forward ref on text field

--- packages/react/src/forms/field-text.tsx
+++ packages/react/src/forms/field-text.tsx
@@ -7,29 +7,28 @@ import {
 } from 'react-hook-form';
 import { classNames } from '../utils';
 import { FieldBase, FieldBaseProps } from './field-base';
-import React, { InputHTMLAttributes } from 'react';
+import { forwardRef, InputHTMLAttributes } from 'react';
 
 type InputTextProps = {
   error?: string;
 } & InputHTMLAttributes<HTMLInputElement>;
 
-export const InputText: React.FC<InputTextProps> = ({
-  error,
-  className = '',
-  ...props
-}) => (
-  <input
-    type='text'
-    className={classNames(
-      'block w-full rounded-md border-0 py-1.5 dark:bg-white/5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset placeholder:text-gray-400 dark:placeholder:text-initial focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
-      error
-        ? 'ring-red-500 focus:ring-red-500'
-        : 'ring-gray-300 dark:ring-white/10 focus:ring-primary-600 dark:focus:ring-primary-500',
-      'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200',
-      className
-    )}
-    {...props}
-  />
+export const InputText = forwardRef<HTMLInputElement, InputTextProps>(
+  ({ error, className = '', ...props }, ref) => (
+    <input
+      ref={ref as any}
+      type='text'
+      className={classNames(
+        'block w-full rounded-md border-0 py-1.5 dark:bg-white/5 text-gray-900 dark:text-white shadow-sm ring-1 ring-inset placeholder:text-gray-400 dark:placeholder:text-initial focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6',
+        error
+          ? 'ring-red-500 focus:ring-red-500'
+          : 'ring-gray-300 dark:ring-white/10 focus:ring-primary-600 dark:focus:ring-primary-500',
+        'disabled:cursor-not-allowed disabled:bg-gray-50 disabled:text-gray-500 disabled:ring-gray-200',
+        className
+      )}
+      {...props}
+    />
+  )
 );
 
 type FieldTextProps<

253d81ce8e03eb2c5545276e6fd8d9590fad3aef
Justin Rahardjo <justindra91@gmail.com>
2024-10-10 11:53:29 -0700

    chore: remove exports from index

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,1 @@
-export * from './alerts';
-export * from './app';
-export * from './auth';
-export * from './button';
-export * from './empty';
-export * from './landing-page';
-export * from './modal';
-export * from './utils';
+export {};
